f14fc25d167aeb172be158bb3fbbc4c1e8e8d12d
xgnid <e19940604@gmail.com>
2017-01-08 23:08:09 +0800

    fix notify css and coursedetail css

--- resources/assets/js/layout/sideMenu.jsx
+++ resources/assets/js/layout/sideMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ModalDispatcher from '../dispatchers/modals';
-
+import UserStore from '../stores/user';
+UserStore.load();
 let SideMenu = React.createClass({
   'handleClickAdd': function (e) {
     ModalDispatcher.dispatch({ 'actionType': 'modal-show', 'data': 'add' });
@@ -13,6 +14,25 @@ let SideMenu = React.createClass({
   'handleClickLink': function (e) {
     ModalDispatcher.dispatch({ 'actionType': 'modal-show', 'data': 'link' });
   },
+
+  'onLoadUserStatus': function () {
+    if ( UserStore.isLogin() ) {
+      if ( UserStore.isLinkFacebook() ) {
+        this.setState({ 'isLogin': true, 'userName': UserStore.getUserName(), 'imageUrl': UserStore.getImageUrl() });
+      }
+      else {
+        this.setState({ 'isLogin': true, 'userName': UserStore.getUserName() });
+      }
+    }
+    else {
+      this.setState({ 'isLogin': false });
+    }
+  },
+
+  'componentWillMount': function () {
+    UserStore.onLoad(this.onLoadUserStatus);
+  },
+
   'render': function () {
     return (
       <div id="sideMenu" className="m-only">
@@ -20,14 +40,14 @@ let SideMenu = React.createClass({
 
           <div id="sideProfile" >
             <img className="img-circle" src="/img/no-user-image.gif" />
-            <span>xgnid</span>
+            <span>{ UserStore.getUserName() }</span>
           </div>
 
           <div className="sideRow" onClick={this.handleClickAdd}>新增課程</div>
           <div className="sideRow" onClick={this.handleClickConfig}>通知設定</div>
           <div className="sideRow" onClick={this.handleClickLink}>帳號連結</div>
 
-          <img id="sideBg" src="/Cyinf/img/CyinfLogo.png" />
+          <img id="sideBg" className="sideBg" src="/Cyinf/img/CyinfLogo.png" />
 
         </div>
       </div>
